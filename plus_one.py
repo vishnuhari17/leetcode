92f223c50aeb2d8a1c7726724c9c55de409f9c22
Vishnuhari-17 <vishnuhari.anup@gmail.com>
2022-10-09 23:06:40 +0530

    Added code

--- plus_one.py
+++ plus_one.py
@@ -1,18 +1,19 @@
-class Solution(object):
-    def plusOne(self, digits):
-        """
-        :type digits: List[int]
-        :rtype: List[int]
-        """
-        if digits[-1] != 9:
-            digits[-1] += 1
-        if digits[-1] == 9 and len(digits) == 1:
-            digits[-1] = 1
-            digits.append(0)
-        else:
-            x =-1
-            while x > -len(digits) and x !=9:
-                digits[x] = 0
-                digits[x-1] += 1
-                x -=1
-        return digits
+digits = [1,2,3]
+length = len(digits)
+value = 0
+multiplier = 10 ** (length- 1)
+i = 0
+while length != 0 and i != length:
+    value += int(digits[i] * multiplier)
+    multiplier /= 10
+    i += 1
+value += 1
+multi = 1
+while value >= multi * 10:
+    multi *= 10
+digits = []
+while int(multi) != 0:
+    digits.append(value // multi)
+    value = value % multi
+    multi = multi // 10
+print(digits)
