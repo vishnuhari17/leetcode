357bea43c94716272645947200770b21d5898fbe
Vishnuhari-17 <vishnuhari.anup@gmail.com>
2022-10-13 23:42:46 +0530

    Modified code draft

--- largest_perimeter_triangle.py
+++ largest_perimeter_triangle.py
@@ -2,8 +2,3 @@ nums=[1,2,3,4,5]
 value = 0
 sumval = 0
 num = []
-for i in range(0,len(nums)-2):
-    for j in range(0,len(nums)-1):
-        for k in range(0,len(nums)):
-            num=[nums[i],nums[j],nums[k]]
-            print(num)
