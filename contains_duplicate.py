309bd9a5978c8b0a861997d33429f25c2a37f93c
Vishnuhari-17 <vishnuhari.anup@gmail.com>
2022-10-26 22:46:34 +0530

    Added code

---
+++ contains_duplicate.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        temp = []
+        for i in nums:
+            if i not in temp:
+                temp.append(i)
+            else:
+                return True
+        return False
