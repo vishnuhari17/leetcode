6e59e10da6f2ed418bb620bd8aeeb8d9aad87cad
Vishnuhari-17 <vishnuhari.anup@gmail.com>
2022-11-12 23:10:26 +0530

    Added code

---
+++ intersection_of_two_arrays.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        list = []
+        for i in nums1:
+            if i in nums2:
+                if i not in list:
+                    list.append(i)
+                nums2.remove(i)
+        return list
