454c9aca7e2ced9a5f80c22d90f008f9c054516b
Vishnuhari-17 <vishnuhari.anup@gmail.com>
2022-12-04 23:50:17 +0530

    Added code

---
+++ plus_minus_hr.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'plusMinus' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def plusMinus(arr):
+    pos=0
+    neg=0
+    zero=0
+    length=len(arr)
+    for i in arr:
+        if i>0:
+            pos+=1
+        elif i<0:
+            neg+=1
+        elif i==0:
+            zero+=1
+    pos1=pos/length
+    neg1=neg/length
+    zero1=zero/length
+    print("{:.6f}".format(pos1))
+    print("{:.6f}".format(neg1))
+    print("{:.6f}".format(zero1))
+    # Write your code here
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+    arr = list(map(int, input().rstrip().split()))
+
+    plusMinus(arr)
