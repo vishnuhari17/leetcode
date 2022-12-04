a3175ca6228d8a3c8a7a346e209a2c2b3484dc3e
Vishnuhari-17 <vishnuhari.anup@gmail.com>
2022-12-04 23:48:55 +0530

    Added code

---
+++ compare_the_triplets_hr.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'compareTriplets' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY a
+#  2. INTEGER_ARRAY b
+#
+
+def compareTriplets(a, b):
+    apoint=0
+    bpoint=0
+    for i in range(0,3):
+        if a[i]>b[i]:
+            apoint+=1
+        elif a[i]<b[i]:
+            bpoint+=1
+    return(apoint,bpoint)
+    # Write your code here
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
